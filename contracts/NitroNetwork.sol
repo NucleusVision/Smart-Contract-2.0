9bce8a8769caaa16f9655477bd7a45c4e5ec6a2d
MailRajesh <70180525+DeFiMonk@users.noreply.github.com>
2022-01-25 16:57:07 +0800

    fix

--- contracts/NitroNetwork.sol
+++ contracts/NitroNetwork.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
@@ -11,128 +12,43 @@ contract NitroNetwork is
   Initializable,
   ERC20Upgradeable,
   ERC20BurnableUpgradeable,
-  PausableUpgradeable,
+  ERC20CappedUpgradeable,
   AccessControlUpgradeable
 {
-  bytes32 public constant GOVERNANCE_ROLE = keccak256("GOVERNANCE_ROLE");
-  bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
-  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-  // 500 Million per Mint
-  uint256 public maxPerMint;
-
-  mapping(address => bool) private isBlackListed;
-  event AddedBlackList(address _who, uint256 _when);
-  event RemovedBlackList(address _who, uint256 _when);
-  event RecoverBlackFunds(
-    address _from,
-    address _to,
-    uint256 _howmuch,
-    uint256 _when
-  );
-  event BurnBlackFunds(address _from, uint256 _howmuch, uint256 _when);
-
-  function initialize(
-    address _admin,
-    address _governance,
-    address _operator,
-    address _pauser,
-    address _minter
-  ) public initializer {
+  function initialize() public initializer {
     __ERC20_init("NitroNetwork", "NITRO");
     __ERC20Burnable_init();
-    __Pausable_init();
+    __ERC20Capped_init(250 * 10**9 * 10**18);
     __AccessControl_init();
-    _setupRole(DEFAULT_ADMIN_ROLE, _admin);
-    _setupRole(GOVERNANCE_ROLE, _governance);
-    _setupRole(OPERATOR_ROLE, _operator);
-    _setupRole(PAUSER_ROLE, _pauser);
-    _setupRole(MINTER_ROLE, _minter);
-    maxPerMint = 500 * 10**6 * 10**18;
-  }
-
-  function pause() public onlyRole(PAUSER_ROLE) {
-    _pause();
-  }
-
-  function unpause() public onlyRole(PAUSER_ROLE) {
-    _unpause();
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _setupRole(MINTER_ROLE, msg.sender);
   }
 
   function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-    require(amount <= maxPerMint, "Mint:: Max limit reached per mint");
     _mint(to, amount);
   }
 
-  function addBlackList(address _who) public onlyRole(OPERATOR_ROLE) {
-    _updateBlackList(_who, true);
-    emit AddedBlackList(_who, block.number);
-  }
-
-  function removeBlackList(address _who) public onlyRole(OPERATOR_ROLE) {
-    _updateBlackList(_who, false);
-    emit RemovedBlackList(_who, block.number);
-  }
-
-  function getBlackListStatus(address _who) public view returns (bool) {
-    return isBlackListed[_who];
-  }
-
-  function recoverBlackFunds(
-    address _from,
-    address _to,
-    uint256 _howmuch
-  ) public onlyRole(GOVERNANCE_ROLE) {
-    require(
-      _from != address(0),
-      "RecoverBlackFunds: Transfer from the zero address"
-    );
-    require(
-      _to != address(0),
-      "RecoverBlackFunds: Transfer to the zero address"
-    );
-    require(
-      getBlackListStatus(_from),
-      "RecoverBlackFunds:: User must be Blacklisted before transfer"
-    );
-    _updateBlackList(_from, false);
-    _transfer(_from, _to, _howmuch);
-    _updateBlackList(_from, true);
-    emit RecoverBlackFunds(_from, _to, _howmuch, block.timestamp);
-  }
-
-  function burnBlackFunds(address _from, uint256 _howmuch)
-    public
-    onlyRole(GOVERNANCE_ROLE)
+  function _mint(address to, uint256 amount)
+    internal
+    override(ERC20Upgradeable, ERC20CappedUpgradeable)
   {
-    require(
-      _from != address(0),
-      "BurnBlackFunds: Transfer from the zero address"
-    );
-    require(
-      getBlackListStatus(_from),
-      "BurnBlackFunds:: User must be Blacklisted before transfer"
-    );
-    _updateBlackList(_from, false);
-    _burn(_from, _howmuch);
-    _updateBlackList(_from, true);
-    emit BurnBlackFunds(_from, _howmuch, block.timestamp);
+    super._mint(to, amount);
   }
 
-  function _updateBlackList(address _wallet, bool _status) internal {
-    isBlackListed[_wallet] = _status;
+  function _burn(address account, uint256 amount)
+    internal
+    override(ERC20Upgradeable)
+  {
+    super._burn(account, amount);
   }
 
   function _beforeTokenTransfer(
     address from,
     address to,
     uint256 amount
-  ) internal override whenNotPaused {
-    require(
-      !getBlackListStatus(from),
-      "BeforeTokenTransfer:: User blacklisted"
-    );
+  ) internal override(ERC20Upgradeable) {
     super._beforeTokenTransfer(from, to, amount);
   }
 }
--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -2,15 +2,5 @@ const NitroNetwork = artifacts.require("NitroNetwork");
 const { deployProxy } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer, accounts) {
-  await deployProxy(
-    NitroNetwork,
-    [
-      "0x4d09c1eBa78c6f8EC4bB443F949118C9c5C2ad3B", // "admin_address",
-      "0x4d09c1eBa78c6f8EC4bB443F949118C9c5C2ad3B", //"governance_address",
-      "0x4d09c1eBa78c6f8EC4bB443F949118C9c5C2ad3B", //"operator_address",
-      "0x4d09c1eBa78c6f8EC4bB443F949118C9c5C2ad3B", //"pauser_address",
-      "0x4d09c1eBa78c6f8EC4bB443F949118C9c5C2ad3B", //"minter_address"
-    ],
-    { deployer, unsafeAllowCustomTypes: true }
-  );
+  await deployProxy(NitroNetwork, { deployer, unsafeAllowCustomTypes: true });
 };
